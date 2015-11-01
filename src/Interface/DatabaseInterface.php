5220bd1f1048147df4880bd33001103ab0f17be6
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-01 08:49:52 +0100

    Modified methods

--- src/Interface/DatabaseInterface.php
+++ src/Interface/DatabaseInterface.php
@@ -8,23 +8,13 @@ interface DatabaseInterface
 
     public function disconnect();
 
-    public function query($query);
-
-    public function fetch();
-
-    function select($table, $conditions = ”, $fields = ‘*’, $order = ”, $limit = null, $offset = null);
+    function select($table, $field = NULL, $value = NULL);
 
     function insert($table, array $data);
 
     function update($table, array $data, $conditions);
 
     function delete($table, $conditions);
-
-    function getInsertId();
-
-    function countRows();
-
-    function getAffectedRows();
 }
 
 
