bf4fd8debeec68cc2fdcfb47ea449c9a2fa4b5a6
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-02 12:47:15 +0100

    updated the connect method to static and properties to static

--- src/Base/DatabaseConnector.php
+++ src/Base/DatabaseConnector.php
@@ -8,26 +8,26 @@ use PDOException;
 use \InvalidArgumentException;
 
 class DatabaseConnector implements DatabaseInterface{
-    protected $dsn = [];
-    protected $database;
-    protected $host;
-    protected $username;
-    protected $password;
-    protected $driver;
+    protected static $dsn = [];
+    protected static $database;
+    protected static $host;
+    protected static $username;
+    protected static $password;
+    protected static $driver;
 
-    public function connect(){
+    public static function connect(){
 
         $envReader = new EnvReader();
         $envReader->load();
 
-        $this->database = getenv('DB_DATABASE');
-        $this->host = getenv('DB_host');
-        $this->username = getenv('DB_USERNAME');
-        $this->password = getenv('DB_PASSWORD');
-        $this->driver = getenv('DB_CONNECTION');
+        self::$database = getenv('DB_DATABASE');
+        self::$host = getenv('DB_host');
+        self::$username = getenv('DB_USERNAME');
+        self::$password = getenv('DB_PASSWORD');
+        self::$driver = getenv('DB_CONNECTION');
 
-        $this->dsn = [$this->database, $this->host, $this->username, $this->password, $this->driver];
-        list($database, $host, $username, $password, $driver) = $this->dsn;
+        self::$dsn = [self::$database, self::$host, self::$username, self::$password, self::$driver];
+        list($database, $host, $username, $password, $driver) = self::$dsn;
 
         try {
             $cxn = new PDO("$driver:dbname=$database;host=$host", $username, $password,[PDO::ATTR_PERSISTENT => true] );
@@ -46,7 +46,7 @@ class DatabaseConnector implements DatabaseInterface{
     public function select($table, $field=NULL, $value=NULL){
     }
     public function insert($table, array $data){
-        var_dump($this->properties);
+
     }
     public function update($table, array $data, $conditions){}
     public function delete($table, $conditions){}
