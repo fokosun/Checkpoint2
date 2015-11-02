05cadb2b848309f621a3a23a6afd2149e022cf29
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-02 12:44:13 +0100

    Test save function

--- index.php
+++ index.php
@@ -6,14 +6,19 @@ require "vendor/autoload.php";
 use Florence\User;
 
 
+//$user = User::find(4);
+////$user->first_name = "Prosper";
+//
+//print_r($user);
+
 $user = new User();
-
-
-$user->first_name = "Ibraheem";
-$user->last_name = "Adeniyi";
-$user->stack = "PHPLaravel";
+$user->first_name = "Shneal";
+$user->last_name = "Obama";
+$user->stack = "None";
 
 $user->save();
+
+
 
 
 
