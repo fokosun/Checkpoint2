26931777ecbff775b7e1f581d2bffbd147326c5c
andela-fokosun <okosunuzflorence@gmail.com>
2015-11-03 15:05:37 +0100

    Testing PotatoTest Class against Travis build

--- tests/PotatoTest.php
+++ tests/PotatoTest.php
@@ -9,11 +9,16 @@ class PotatoTest extends \PHPUnit_Framework_TestCase
 {
     public function testDestroy()
     {
-        $this->assertTrue(Car::destroy(23));
-        $this->assertTrue(User::destroy(4));
+        $this->assertTrue(Car::destroy(20));
+        $this->assertTrue(User::destroy(7));
     }
 
     public function testSave()
+    {
+
+    }
+
+    public function tearDown()
     {
 
     }
